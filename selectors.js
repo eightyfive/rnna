bb120358fe2467da66a07274976e760b652542ee
Benoit Sagols <benoit.sagols@gmail.com>
2020-03-28 20:25:19 +0700

    Add selectors

---
+++ selectors.js
@@ -0,0 +1,43 @@
+import _get from 'lodash.get';
+import { createSelector as create } from 'reselect';
+
+const cache = {};
+
+export function getSlice(name) {
+  if (!cache[name]) {
+    cache[name] = state => _get(state, name);
+  }
+
+  return cache[name];
+}
+
+export function createSelector(...names) {
+  const original = names.some(name => typeof name !== 'string');
+
+  if (original) {
+    return create(...names);
+  }
+
+  const selector = names.pop();
+  const slices = names.map(name => getSlice(name));
+
+  return create(...slices, selector);
+}
+
+function getResult(byId, result) {
+  if (Array.isArray(result)) {
+    return result.map(id => byId[id]);
+  }
+
+  return byId[result] || null;
+}
+
+export function createResultSelector(slice1, slice2) {
+  return createSelector(slice1, slice2, getResult);
+}
+
+export function createRelationSelector(find, related, foreign) {
+  return create(find, getSlice(related), (entity, byId) =>
+    getResult(byId, _get(entity, foreign)),
+  );
+}
