8a1876f53a5f8b19bc4213e886a08f061f90641b
Benoit Sagols <benoit.sagols@gmail.com>
2020-03-28 18:57:07 +0700

    Add mapApi operator

--- operators.js
+++ operators.js
@@ -42,6 +42,7 @@ export const ofHTTPErrorType = status => source =>
 export const takeUntilType = (action$, type) => source =>
   source.pipe(takeUntil(action$.pipe(ofType(type))));
 
+// Actions
 export const mapAction = (typeCreator, payloadCreator) => source =>
   source.pipe(
     map(data => {
@@ -82,4 +83,81 @@ export const startWithAction = (type, payload) => source =>
       type,
       payload,
     }),
+  );
+
+// Api
+export const mapApi = (api, method, urlCreator, dataCreator) => source =>
+  source.pipe(
+    switchMap(action => {
+      let payload = action.payload || action;
+
+      let url;
+
+      if (typeof urlCreator === 'function') {
+        url = urlCreator(payload);
+      } else {
+        url = urlCreator;
+      }
+
+      let data;
+
+      if (dataCreator) {
+        if (typeof dataCreator === 'function') {
+          data = dataCreator(payload);
+        } else {
+          data = dataCreator;
+        }
+      }
+
+      let res$;
+
+      if (data) {
+        res$ = api[method.toLowerCase()](url, data);
+      } else {
+        res$ = api[method.toLowerCase()](url);
+      }
+
+      const type = `[API] ${method.toUpperCase()} /${url}`;
+
+      return res$.pipe(
+        filter(res => res.headers.get('Content-Type') === 'application/json'),
+        mapApiResponse(type),
+        startWithAction(`${type} 202`),
+        catchApiError(type),
+      );
+    }),
+  );
+
+const mapApiResponse = type => source =>
+  source.pipe(
+    switchMap(res =>
+      from(
+        res.json().then(json => ({
+          type: `${type} ${res.status}`,
+          payload: json.data || json,
+        })),
+      ),
+    ),
+  );
+
+const catchApiError = type => source =>
+  source.pipe(
+    catchError(err =>
+      of(err).pipe(
+        filter(err => err.response),
+        switchMap(err =>
+          from(
+            err.response.json().then(data => {
+              err.data = data;
+
+              return {
+                type: `${type} ${err.response.status}`,
+                error: true,
+                payload: err,
+              };
+            }),
+          ),
+        ),
+      ),
+    ),
   );
