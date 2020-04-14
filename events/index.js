b023d688d17341e48002e6a9719cdcb0c138edc3
Benoit Sagols <benoit.sagols@gmail.com>
2020-04-14 22:38:02 +0700

    fromAppState

--- events/index.js
+++ events/index.js
@@ -1,21 +1,8 @@
 import { AppState } from 'react-native';
-import { fromEventPattern } from 'rxjs';
-import { filter, switchMap } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
-import { mapAction, ofType } from '../operators';
-
-export const appState = action$ =>
-  action$.pipe(
-    ofType('[App] Storage synched'),
-    switchMap(() =>
-      fromEventPattern(
-        handler => AppState.addEventListener('change', handler),
-        handler => AppState.removeEventListener('change', handler),
-      ).pipe(
-        filter(state => /(active|background)/.test(state)),
-        mapAction(state =>
-          state === 'active' ? '[App] Foreground' : '[App] Background',
-        ),
-      ),
-    ),
+export const fromAppState = source =>
+  source.pipe(
+    switchMap(() => fromEvent(AppState, 'change'),
   );
