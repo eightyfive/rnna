91d05835fe259b535689c580c2642fac6c9884f2
Benoit Sagols <benoit.sagols@gmail.com>
2020-04-16 10:16:56 +0700

    Adapt logger to simple stream

--- packages/http/use/logger.js
+++ packages/http/use/logger.js
@@ -1,52 +1,50 @@
-import { from, merge } from 'rxjs';
-import { skip, take, mapTo, switchMap, tap } from 'rxjs/operators';
+import { from } from 'rxjs';
+import { mapTo, switchMap, tap, withLatestFrom } from 'rxjs/operators';
 
-const logRed = (err, logger = log) => logger(`%c${err}`, 'color: red');
+const logRed = (err, logger = log) => logger;
 const groupRed = err => logRed(err, console.group);
 
-const logger = next => req => {
-  const rr$ = next(req);
-
-  // Req
-  const req$ = rr$.pipe(
-    take(1),
-    tap(req => {
-      console.groupCollapsed(req.method, req.url, 202);
-
-      console.log(req.method);
-      console.log(req.url);
-      logHeaders(req.headers);
-      console.log('credentials', req.credentials);
-      console.log('mode', req.mode);
-
-      console.groupEnd();
-    }),
-  );
-
-  // Res
-  const res$ = rr$.pipe(
-    skip(1),
-    switchMap(res => {
+const logger = next => req$ => {
+  return next(req$).pipe(
+    withLatestFrom(req$),
+    switchMap(([res, req]) => {
       return from(res.clone().json()).pipe(
         tap(data => {
           if (res.status >= 300) {
-            groupRed(`${req.method} ${req.url} ${res.status}`);
+            console.group(
+              `%c${[req.method, req.url, res.status].join(' ')}`,
+              'color: red',
+            );
           } else {
             console.groupCollapsed(req.method, req.url, res.status);
           }
+
+          // Request
+          console.groupCollapsed('Request');
+
+          console.log(req.method);
+          console.log(req.url);
+          logHeaders(req.headers);
+          console.log('credentials', req.credentials);
+          console.log('mode', req.mode);
+
+          console.groupEnd();
+
+          // Response
+          console.groupCollapsed('Response');
 
           console.log(res.statusText, res.status);
           console.log(data);
           logHeaders(res.headers);
 
           console.groupEnd();
+
+          console.groupEnd();
         }),
+        mapTo(res),
       );
     }),
-    mapTo(res$),
   );
-
-  return merge(req$, res$);
 };
 
 export default logger;
--- packages/http/use/logger.test.js
+++ packages/http/use/logger.test.js
@@ -1,11 +1,13 @@
 import Http from '../index';
 import logger from './logger';
 
-const spyGroup = jest
+const groupCollapsed = jest
   .spyOn(global.console, 'groupCollapsed')
   .mockImplementation();
 
-const spyLog = jest.spyOn(global.console, 'log').mockImplementation();
+const group = jest.spyOn(global.console, 'group').mockImplementation();
+
+const log = jest.spyOn(global.console, 'log').mockImplementation();
 
 let api;
 
@@ -22,12 +24,33 @@ beforeEach(() => {
 });
 
 describe('logger', () => {
-  it('logs req & res', done => {
+  it('logs HTTP 200', done => {
     fetch.mockResponse('{"foo": "bar"}');
 
-    api.get('api/resource').subscribe(() => {
-      expect(spyGroup).toHaveBeenCalled();
-      expect(spyLog).toHaveBeenCalled();
+    api.get('api/resource').subscribe(res => {
+      expect(res.ok).toBe(true);
+      expect(res.status).toBe(200);
+
+      expect(groupCollapsed).toHaveBeenCalledTimes(5);
+      expect(log).toHaveBeenCalledTimes(8);
+
+      done();
+    });
+  });
+
+  it('logs HTTP 500', done => {
+    fetch.mockResponse('{"foo": "bar"}', {
+      status: 500,
+      statusText: 'Unprocessable Entity',
+    });
+
+    api.get('api/resource').subscribe(res => {
+      expect(res.ok).toBe(false);
+      expect(res.status).toBe(500);
+
+      expect(group).toHaveBeenCalledTimes(1);
+      expect(groupCollapsed).toHaveBeenCalledTimes(4);
+      expect(log).toHaveBeenCalledTimes(8);
 
       done();
     });
