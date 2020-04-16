8e552d162fc9a5641a2e93e7375e1e86ed60ef03
Benoit Sagols <benoit.sagols@gmail.com>
2020-04-16 10:16:44 +0700

    Adapt error to simple stream

--- packages/http/use/error.js
+++ packages/http/use/error.js
@@ -1,25 +1,17 @@
-import { merge } from 'rxjs';
-import { skip, take, tap } from 'rxjs/operators';
+import { map, tap, withLatestFrom } from 'rxjs/operators';
 
 import HTTPError from 'fetch-run/http-error';
 
-const error = next => req => {
-  const rr$ = next(req);
-
-  // Req
-  const req$ = rr$.pipe(take(1));
-
-  // Res
-  const res$ = rr$.pipe(
-    skip(1),
-    tap(res => {
+const error = next => req$ => {
+  return next(req$).pipe(
+    withLatestFrom(req$),
+    tap(([res, req]) => {
       if (!res.ok) {
         throw new HTTPError(res.status, res.statusText, req, res.clone());
       }
     }),
+    map(([res]) => res),
   );
-
-  return merge(req$, res$);
 };
 
 export default error;
