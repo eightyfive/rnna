5e67471bd135225edca8b0dd1998d3212848926b
eightyfive <benoit.sagols@gmail.com>
2020-05-06 12:40:18 +0700

    Simple RN template

---
+++ template/App.js
@@ -0,0 +1,152 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type { Node } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  // useColorScheme,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({ children, title }): Node => {
+  // const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = false;
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}
+      >
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}
+      >
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = ({ componentId, nav }) => {
+  // const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = false;
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const hermes = global.HermesInternal ? (
+    <View style={styles.engine}>
+      <Text
+        style={[
+          styles.footer,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}
+      >
+        Engine: Hermes
+      </Text>
+    </View>
+  ) : null;
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}
+      >
+        <Header />
+        {hermes}
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}
+        >
+          <Section title="Test navigation" />
+          <View style={{ paddingHorizontal: 24 }}>
+            <TouchableOpacity onPress={() => nav.push('Test', {}, componentId)}>
+              <Text>Go to Test screen</Text>
+            </TouchableOpacity>
+          </View>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
--- template/index.js
+++ template/index.js
@@ -1,3 +1,28 @@
-import app from './app/boot';
+/**
+ * @format
+ */
 
-app.run();
+import React from 'react';
+import { Text } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import { createStackNavigator } from '@rnna/navigator';
+import { name as appName } from './app.json';
+import App from './App';
+
+// Screens
+const Test = () => <Text style={{ padding: 24 }}>Test screen</Text>;
+
+Navigation.registerComponent('App', () => App);
+Navigation.registerComponent('Test', () => Test);
+
+// Navigator
+const navigator = createStackNavigator({
+  App: { title: appName },
+  Test: { title: 'Test screen' },
+});
+
+Navigation.events().registerAppLaunchedListener(() =>
+  navigator.mount({
+    nav: navigator,
+  }),
+);
