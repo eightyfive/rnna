103d2eeb7f743f80bbe60f878ebfb2d2b14b0c06
eightyfive <benoit.sagols@gmail.com>
2020-08-06 14:32:52 +0700

    [rx] Add `on` epic helpers

--- packages/rx/index.js
+++ packages/rx/index.js
@@ -1,3 +1,24 @@
 import { of } from 'rxjs';
 
+import { isHTTPErrorType, isType } from './operators';
+
 export const ofAction = (type, payload) => of({ type, payload });
+
+export function on(...types) {
+  const handler = types.pop();
+
+  return (action$, state$, services) => {
+    return action$.pipe(
+      isType(...types),
+      handler(action.payload || action, state$.value, services),
+    );
+  };
+}
+
+export function onHTTP(code, handler) {
+  return (action$, state$, services) =>
+    action$.pipe(
+      isHTTPErrorType(code),
+      handler(action.payload || action, state$.value, services),
+    );
+}
