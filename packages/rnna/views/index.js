004e9ab0ab8643fc5b1a10a5f4ec28199e10ce9e
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-01-04 18:29:38 +0100

    [app] Add spacing utilities (views)

---
+++ packages/rnna/views/index.js
@@ -0,0 +1,9 @@
+export {
+  makeCol as createCol,
+  makeRow as createRow,
+} from 'react-native-col/make';
+export {
+  createMargin,
+  createPadding,
+  createView as createSpacedView,
+} from './spacing';
