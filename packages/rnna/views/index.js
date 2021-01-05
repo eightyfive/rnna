a009f577516f43e57729553faa61215ddf8f1dbf
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-01-05 09:15:56 +0100

    Remove react-native-spacing view

--- packages/rnna/views/index.js
+++ packages/rnna/views/index.js
@@ -1,9 +1,1 @@
-export {
-  makeCol as createCol,
-  makeRow as createRow,
-} from 'react-native-col/make';
-export {
-  createMargin,
-  createPadding,
-  default as createSpacedView,
-} from 'react-native-spacing';
+export { createMargin, createPadding } from 'react-native-spacing';
