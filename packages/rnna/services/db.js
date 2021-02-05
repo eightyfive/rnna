d1e091034fa6e0b2a86454a742138d47d2c5df0d
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-02-05 09:55:45 +0100

    Add paylod default empty value

--- packages/rnna/services/db.js
+++ packages/rnna/services/db.js
@@ -8,7 +8,7 @@ const initialState = {
 
 export function createDbReducer(reducer, getOrderName = i => i) {
   return produce((draft, action) => {
-    const { entities, result } = action.payload;
+    const { entities, result } = action.payload || {};
 
     if (entities) {
       produceTables(draft.tables, entities);
