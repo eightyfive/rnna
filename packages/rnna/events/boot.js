04ffb00cb4002d533386493fad92b65eafd81166
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-03-08 10:22:45 +0100

    Rollback Boot and Register events

--- packages/rnna/events/boot.js
+++ packages/rnna/events/boot.js
@@ -1,9 +1,11 @@
-import { switchMap, take } from 'rxjs/operators';
+import { filter, switchMap, take } from 'rxjs/operators';
+import { REHYDRATE } from 'redux-persist';
 
 export default function createOnBoot(handler) {
   return (action$, state$, services) =>
     action$.pipe(
+      filter(({ type }) => type === REHYDRATE),
       take(1),
-      switchMap(() => handler(services)),
+      switchMap(() => handler(state$.value, services)),
     );
 }
