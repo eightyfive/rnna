b425a4a5a6631f6fd838ff7d90d33d452a40bc42
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-04-16 14:32:50 +0800

    [app] Add onAction event helper

--- packages/rnna/events/index.js
+++ packages/rnna/events/index.js
@@ -1,4 +1,11 @@
+import { mergeMap } from 'rxjs/operators';
+
 export { default as onAppState } from './app-state';
 export { default as onBoot } from './boot';
 export { default as onRegister } from './register';
 export { default as router } from './router';
+
+export function onAction(handler) {
+  return (action$, state$, services) =>
+    action$.pipe(mergeMap(action => handler(action, state$.value, services)));
+}
