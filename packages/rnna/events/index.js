cfa7c57392267df45438aa84fa797a37202e7d7d
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-04-16 14:32:28 +0800

    Centralize events helpers

---
+++ packages/rnna/events/index.js
@@ -0,0 +1,4 @@
+export { default as onAppState } from './app-state';
+export { default as onBoot } from './boot';
+export { default as onRegister } from './register';
+export { default as router } from './router';
