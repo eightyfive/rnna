5446c5c203c85ad6dc3a46973f7ac05e327aa41b
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-04-18 18:11:39 +0800

    [app] Move actions HTTP middleware out

--- packages/rnna/providers/api.js
+++ packages/rnna/providers/api.js
@@ -1,14 +1,5 @@
 import produce from 'immer';
 import { createSelector } from 'reselect';
-import parseUrl from 'url-parse';
-import { from, merge, of } from 'rxjs';
-import {
-  catchError,
-  filter,
-  map,
-  switchMap,
-  withLatestFrom,
-} from 'rxjs/operators';
 import Http from '@rnna/http';
 import * as uses from '@rnna/http/use';
 
@@ -22,11 +13,8 @@ class ApiProvider extends Provider {
   }
 
   register(services, reducers, epics) {
-    // Error middleware to throw HTTP errors (>= 400)
     this.http.use(uses.error);
-
-    // Emits req, res & err actions
-    this.http.use(emitActions);
+    this.http.use(uses.actions);
 
     Object.assign(services, { api: this.http });
 
@@ -41,86 +29,6 @@ class ApiProvider extends Provider {
     });
   }
 }
-
-const emitActions = next => req$ => {
-  const res$ = next(req$);
-
-  const reqAction$ = req$.pipe(
-    map(req => {
-      const { pathname } = parseUrl(req.url);
-
-      // Req action
-      return {
-        type: pathname.substring(1),
-        meta: {
-          req,
-          res: null,
-        },
-      };
-    }),
-  );
-
-  const resAction$ = res$.pipe(
-    filter(res => res.headers.get('Content-Type') === 'application/json'),
-    withLatestFrom(req$),
-
-    // Res action
-    switchMap(([res, req]) =>
-      from(res.json()).pipe(
-        map(json => {
-          const { pathname } = parseUrl(req.url);
-
-          return {
-            type: pathname.substring(1),
-            payload: json.data || json,
-            meta: {
-              req,
-              res,
-            },
-          };
-        }),
-      ),
-    ),
-
-    // Error action
-    catchError(err =>
-      of(err).pipe(
-        filter(err => {
-          if (err.response) {
-            return true;
-          }
-
-          // Re-throw if not HTTP error
-          throw err;
-        }),
-        switchMap(err =>
-          from(err.response.json()).pipe(
-            map(json => {
-              const req = err.request;
-              const res = err.response;
-
-              const { pathname } = parseUrl(req.url);
-
-              err.data = json.data || json;
-
-              return {
-                type: pathname.substring(1),
-                payload: err,
-                error: true,
-                meta: {
-                  req,
-                  res,
-                },
-              };
-            }),
-          ),
-        ),
-      ),
-    ),
-  );
-
-  return merge(reqAction$, resAction$);
-};
 
 // Resource reducer
 const initialState = {
