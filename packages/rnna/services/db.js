2fd5168717c92bab9599e5fa246fb1a359c2dd9b
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-04-18 16:41:30 +0800

    Remove db dependency

--- packages/rnna/services/db.js
+++ packages/rnna/services/db.js
@@ -1,13 +1,31 @@
-import db, { createSelector } from '@rnna/db';
+import { createSelector as reSelector } from 'reselect';
 
-export { createSelector };
+// getSlice
+function getSlice(path) {
+  const { slices } = cache;
 
-const o = Object;
-
-export default function createDb(entities) {
-  for (const [key, schema] of o.entries(entities)) {
-    db.addEntity(key, schema);
+  if (!slices.has(path)) {
+    slices.set(path, st => _get(st, path));
   }
 
-  return db;
+  return slices.get(path);
 }
+
+// createSelector
+export function createSelector(...names) {
+  const selector = names.pop();
+
+  const slices = names.map(name =>
+    typeof name === 'string' || Array.isArray(name) ? getSlice(name) : name,
+  );
+
+  if (!slices.length) {
+    return reSelector(getSlice(selector), s => s);
+  }
+
+  return reSelector(...slices, selector);
+}
+
+const db = {};
+
+export default db;
