ffbc89cb8507f38e8a48720a17dbe9de29d4ea22
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-05-01 15:50:09 +0800

    Add router web

---
+++ packages/bundle-router/Router.web.js
@@ -0,0 +1,57 @@
+export default class Router {
+  constructor(routes, services = {}) {
+    this.routes = routes;
+    this.services = services;
+    this.props = {};
+    this.listeners = [];
+
+    window.addEventListener('popstate', this.handlePopstate);
+  }
+
+  handlePopstate = ev => {
+    const { pathname } = new URL(document.location);
+
+    const path = pathname.substring(1);
+
+    this.triggerChange(path, (ev.state || {}).params || []);
+  };
+
+  addGlobalProp(name, prop) {
+    this.props[name] = prop;
+  }
+
+  go(path, ...params) {
+    window.history.pushState({ params }, null, path);
+
+    this.triggerChange(path, params);
+  }
+
+  triggerChange(path, params) {
+    this.listeners.map(listener => listener(path, params));
+  }
+
+  render(path) {
+    const Screen = this.routes[path] || null;
+
+    if (Screen) {
+      const props = Object.assign(
+        {},
+        this.props,
+        Screen.controller(this.services),
+      );
+
+      return <Screen {...props} />;
+    }
+
+    return null;
+  }
+
+  subscribe(listener) {
+    this.listeners.push(listener);
+
+    return () => {
+      const index = this.listeners.indexOf(listener);
+      this.listeners.splice(index, 1);
+    };
+  }
+}
