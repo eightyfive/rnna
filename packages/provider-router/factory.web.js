a0c128ef8da382fa46ca658e82b52591204ff624
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-05-02 13:17:49 +0800

    Concise code style

--- packages/provider-router/factory.web.js
+++ packages/provider-router/factory.web.js
@@ -1,11 +1,7 @@
 import Router from './Router';
 
 export default function createRouter(routes, db) {
-  const components = new Map();
-
-  for (const [componentId, Component] of Object.entries(routes)) {
-    components.set(componentId, Component);
-  }
+  const components = new Map(Object.entries(routes));
 
   return new Router(routes, components, { db });
 }
