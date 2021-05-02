6fb778751ba7a90b748b3a37094d094f62832e71
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-05-02 09:53:20 +0800

    Export service provider abstract

--- packages/rnna/index.js
+++ packages/rnna/index.js
@@ -1,1 +1,9 @@
 export { default as Container } from 'bottlejs';
+
+export class ServiceProvider {
+  register(container) {
+    throw new Error(`Abstract: Implement ServiceProvider register method`);
+  }
+
+  boot(services, store) {}
+}
