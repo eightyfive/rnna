9af5f978fb8ddf304b7243502a1a85db6b725d50
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-05-03 19:34:41 +0800

    [bundle-api-resource] Remove unused provider

--- packages/bundle-api-resource/index.js
+++ packages/bundle-api-resource/index.js
@@ -1,7 +1,8 @@
 import { Bundle } from 'rnna';
 
 import createReducer from './reducer';
-import ResourceProvider from './provider';
+import Resource from './resource';
+import { createFind, createGet } from './selectors';
 
 export default class ResourceBundle extends Bundle {
   constructor(name) {
@@ -10,8 +11,32 @@ export default class ResourceBundle extends Bundle {
     this.name = name;
   }
 
-  getServiceProvider() {
-    return new ResourceProvider(this.name);
+  register(container) {
+    container.service(
+      this.name,
+      Resource,
+      `${this.name}.endpoint`,
+      `${this.name}.schema`,
+    );
+  }
+
+  boot(services, store) {
+    // Selectors
+    const resource = services[this.name];
+    const entity = resource.schema;
+
+    // TODO: plural, singular for: getUsers, findUser.
+    const Name = this.name;
+    const Names = this.name;
+
+    const relations = Object.values(entity.schema).map(schema =>
+      Array.isArray(schema) ? schema[0].key : schema.key,
+    );
+
+    Object.assign(services.db, {
+      [`find${Name}`]: createFind(entity, relations),
+      [`get${Names}`]: createGet(entity, relations),
+    });
   }
 
   getReducers() {
