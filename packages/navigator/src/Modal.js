c61a3f1f664b8adae634381a1bdc40c671c48dcf
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-05-08 15:24:30 +0800

    Add simple Modal

---
+++ packages/navigator/src/Modal.js
@@ -0,0 +1,19 @@
+import { Navigation } from 'react-native-navigation';
+
+import Stack from './Stack';
+
+export default class Modal extends Stack {
+  mount(initialProps) {
+    Navigation.showModal(this.getInitialLayout(initialProps));
+  }
+
+  unmount() {
+    this.dismiss();
+  }
+
+  dismiss() {
+    const component = this.components.get(this.name);
+
+    Navigation.dismissModal(component.id);
+  }
+}
---
+++ packages/navigator/src/Modal.test.js
@@ -0,0 +1,32 @@
+import { Navigation } from 'react-native-navigation';
+
+import { makeComponent } from './Component.test';
+import { makeStack } from './StackNavigator.test';
+
+import Component from './Component';
+import Stack from './Stack';
+import Modal from './Modal';
+
+let app;
+
+const A = new Component('A', 'A');
+const B = new Component('B', 'B');
+
+beforeEach(() => {
+  app = new Modal({ A, B });
+  app.mount({ foo: 'bar' });
+});
+
+test('mount', () => {
+  expect(app instanceof Stack).toBe(true);
+
+  expect(Navigation.showModal).toHaveBeenCalledWith({
+    stack: makeStack([makeComponent('A', 'A', {}, { foo: 'bar' })]),
+  });
+});
+
+test('unmount (dismiss)', () => {
+  app.unmount();
+
+  expect(Navigation.dismissModal).toHaveBeenCalledWith('A');
+});
