f530f99f156bb5e4a5b91bbe9ccaeba9e8c0d91d
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-05-08 23:18:42 +0800

    [navigator] Remove defined Map

--- packages/navigator/src/Definable.js
+++ packages/navigator/src/Definable.js
@@ -1,14 +1,8 @@
 export default /** abstract */ class Definable {
-  constructor() {
-    this.defined = new Map();
-  }
-
   defineProperty(name, value) {
-    if (this.defined.has(name)) {
+    if (this[name]) {
       throw new Error(`${name} is already defined`);
     }
-
-    this.defined.set(name, true);
 
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty#description
     Object.defineProperty(this, name, { enumerable: true, value });
