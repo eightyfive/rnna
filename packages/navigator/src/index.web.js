c8664ab351b229bb15b0d35fdb51ebd2949d8ad4
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-05-09 12:15:23 +0800

    [navigator] Export missing web parts

--- packages/navigator/src/index.web.js
+++ packages/navigator/src/index.web.js
@@ -1,7 +1,9 @@
+import Registry from './Registry';
 import SwitchNavigator from './SwitchNavigator';
+import * as Layouts from './Layouts';
 import * as Utils from './utils';
 
-export { default as Registry } from './Registry';
+export { Layouts, Registry, SwitchNavigator, Utils };
 
 export function createSwitchNavigator(routes, config = {}) {
   const components = Utils.createComponents(routes);
