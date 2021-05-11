5b26d4bfea2f3651bba503cc37896a4c58cf6e2d
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-05-11 11:10:17 +0800

    Allow for Store bundle.register & boot to be async

--- packages/rnna/store.js
+++ packages/rnna/store.js
@@ -24,7 +24,7 @@ function getHydratedAsync(persistor) {
   });
 }
 
-export default function getStore(
+export default async function getStore(
   {
     epics = [],
     middlewares = [],
@@ -34,14 +34,18 @@ export default function getStore(
   },
   container,
 ) {
-  // Register bundles
-  bundles.forEach(bundle => {
-    bundle.register(container);
+  let jobs;
 
+  // Register bundles
+  jobs = bundles.map(bundle => {
     Object.assign(reducers, bundle.getReducers());
 
     epics.push(...bundle.getEpics());
+
+    return bundle.register(container);
   });
+
+  await Promise.all(jobs);
 
   // Epics
   let rootEpic;
@@ -83,16 +87,14 @@ export default function getStore(
   store.persistor = persistor;
 
   // Boot bundles
-  const whenBooted = Promise.all(
-    bundles.map(bundle => bundle.boot(container.services, store)),
-  );
+  jobs = bundles.map(bundle => bundle.boot(container.services, store));
 
-  store.boot = () => Promise.all(whenHydrated, whenBooted);
+  await Promise.all(jobs);
 
   // Run epics
   if (epicMiddleware) {
     epicMiddleware.run(rootEpic);
   }
 
-  return store;
+  return whenHydrated;
 }
