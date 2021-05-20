b91c0fafbc62fec8b3b4a6b47943b2b7055c999f
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-05-20 18:42:42 +0800

    Throw implement errors with correct constructor name

--- packages/rnna/index.js
+++ packages/rnna/index.js
@@ -2,7 +2,9 @@ export { default as Container } from './container';
 
 export class Bundle {
   register(container) {
-    throw new Error(`Abstract: Implement Bundle::register method`);
+    throw new Error(
+      `Abstract: Implement ${this.constructor.name}::register(container) method`,
+    );
   }
 
   boot(services, store) {
@@ -21,19 +23,19 @@ export class Bundle {
 export class Controller {
   match(path, query = {}) {
     throw new Error(
-      `Implement ${this.constructor.name}.match(path, query = {}) method`,
+      `Implement ${this.constructor.name}::match(path, query = {}) method`,
     );
   }
 }
 
 export class Listener {
   listen(type) {
-    throw new Error(`Implement ${this.constructor.name}.listen(type) method`);
+    throw new Error(`Implement ${this.constructor.name}::listen(type) method`);
   }
 
   handle(payload) {
     throw new Error(
-      `Implement ${this.constructor.name}.handle(payload) method`,
+      `Implement ${this.constructor.name}::handle(payload) method`,
     );
   }
 }
