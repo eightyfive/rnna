762b5605609db7b1f424095d9ce721a652e506e5
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-07-28 12:40:53 +0200

    Make fetching selector dynamic

--- packages/bundle-api/src/selectors.js
+++ packages/bundle-api/src/selectors.js
@@ -2,8 +2,6 @@ import { createSelector } from 'reselect';
 
 const hasPaths = paths => paths.length > 0;
 
-export const fetching = createSelector(state => state.http.fetching, hasPaths);
-
 export const creating = createSelector(state => state.http.creating, hasPaths);
 
 export const reading = createSelector(state => state.http.reading, hasPaths);
@@ -11,3 +9,11 @@ export const reading = createSelector(state => state.http.reading, hasPaths);
 export const updating = createSelector(state => state.http.updating, hasPaths);
 
 export const deleting = createSelector(state => state.http.deleting, hasPaths);
+
+export const fetching = createSelector(
+  creating,
+  reading,
+  updating,
+  deleting,
+  (c, r, u, d) => c || r || u || d,
+);
