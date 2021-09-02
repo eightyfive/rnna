781f0a547b22ded30392a983e15f28b7fddac114
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-09-02 11:36:19 +0200

    [resource] Use schema as key identifier

--- packages/bundle-api-resource/src/index.js
+++ packages/bundle-api-resource/src/index.js
@@ -5,29 +5,27 @@ import Resource from './resource';
 import { createFind, createGet } from './selectors';
 
 export default class ResourceBundle extends Bundle {
-  constructor(name) {
+  constructor(schema) {
     super();
 
-    this.name = name;
+    this.schema = schema;
   }
 
   register(container) {
-    container.service(
-      this.name,
-      Resource,
-      `${this.name}.endpoint`,
-      `${this.name}.schema`,
+    container.factory(
+      this.schema.key,
+      ({ api }) => new Resource(api.endpoint(this.schema.key), this.schema),
     );
   }
 
   boot(services, store) {
     // Selectors
-    const resource = services[this.name];
+    const resource = services[this.schema.key];
     const entity = resource.schema;
 
     // TODO: plural, singular for: getUsers, findUser.
-    const Name = this.name;
-    const Names = this.name;
+    const Name = this.schema.key;
+    const Names = this.schema.key;
 
     const relations = Object.values(entity.schema).map(schema =>
       Array.isArray(schema) ? schema[0].key : schema.key,
@@ -40,7 +38,7 @@ export default class ResourceBundle extends Bundle {
   }
 
   getReducers() {
-    return { [this.name]: createReducer(this.name) };
+    return { [this.schema.key]: createReducer(this.schema.key) };
   }
 
   getEpics() {
