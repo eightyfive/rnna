a91b489ec9a09408c9f2637add1deee607527548
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-09-04 20:03:43 +0200

    [bundle-db] Use Db

--- packages/bundle-db/src/index.js
+++ packages/bundle-db/src/index.js
@@ -1,13 +1,20 @@
 import { Bundle } from 'rnna';
-
-import createDb from './db';
+import Db from '@rnna/db';
 
 export default class DbProvider extends Bundle {
   register(container) {
-    container.service('db', createDb, 'db.selectors');
+    container.service('db', createDb, 'db.*');
   }
 
   boot(services, store) {
     services.db.setStore(store);
   }
+}
+
+function createDb({ selectors }) {
+  const db = new Db();
+
+  Object.assign(db, selectors);
+
+  return db;
 }
