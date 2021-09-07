c3ddc3512ab7381dbfdd29380970444efab7257c
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-09-07 17:56:33 +0200

    [http] Log error response as well

--- packages/http/src/use/log.js
+++ packages/http/src/use/log.js
@@ -1,26 +1,38 @@
-import { filter, tap } from 'rxjs/operators';
+import { catchError, filter, tap } from 'rxjs/operators';
 
 import parse from 'console.unicorn/parser';
 
 const log = next => req$ => {
   return next(req$).pipe(
     filter(({ responseType: type }) => type === 'json'),
-    tap(({ request: req, response: res, status }) => {
-      const label = parse(
-        `{white}${req.method} {grey}${req.url} {white}${status}`,
-      );
-
-      console.groupCollapsed(...label);
-
-      if (req.body) {
-        console.log(JSON.parse(req.body));
+    tap(res => {
+      logGroup(res.request, res.response, res.status);
+    }),
+    catchError(err => {
+      if (err.name === 'AjaxError') {
+        logGroup(err.request, err.response, err.status, true);
       }
 
-      console.log(res);
-
-      console.groupEnd();
+      throw err;
     }),
   );
 };
 
 export default log;
+
+function logGroup(req, res, status, error = false) {
+  const color = error ? 'red' : 'white';
+
+  const label = parse(
+    `{${color}}${req.method} {grey}${req.url} {${color}}${status}`,
+  );
+
+  console.groupCollapsed(...label);
+
+  if (req.body) {
+    console.log(JSON.parse(req.body));
+  }
+
+  console.log(res);
+  console.groupEnd();
+}
