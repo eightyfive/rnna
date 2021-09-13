bb04796d36864f5582035e5e61defd7afda82632
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-09-13 12:59:20 +0200

    [http] Filter only JSON responses for mapping response

--- packages/http/src/use/response.js
+++ packages/http/src/use/response.js
@@ -1,7 +1,10 @@
-import { map } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 
 const response = next => req$ => {
-  return next(req$).pipe(map(res => res.response.data || res.response));
+  return next(req$).pipe(
+    filter(({ responseType: type }) => type === 'json'),
+    map(({ response }) => (response ? response.data || response : response)),
+  );
 };
 
 export default response;
