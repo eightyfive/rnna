c6c45a7b0d9d99e276c955fec63a4bd04e60b25e
0x55 <0xfiftyfive@gmail.com>
2021-12-06 17:59:35 +0100

    [navigator] Make sure Modal in ModalNavigator

--- packages/navigator/src/ModalNavigator.js
+++ packages/navigator/src/ModalNavigator.js
@@ -1,3 +1,12 @@
 import StackNavigator from './StackNavigator';
+import Modal from './Layouts/Modal';
 
-export default class ModalNavigator extends StackNavigator {}
+export default class ModalNavigator extends StackNavigator {
+  constructor(layout) {
+    if (!(layout instanceof Modal)) {
+      throw new TypeError('Layout must be modal');
+    }
+
+    super(layout);
+  }
+}
