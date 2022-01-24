4f20b4b4fc6663a04319ca74139109aebe85c455
0x55 <0xfiftyfive@gmail.com>
2022-01-24 14:08:06 +0100

    [app] Dispatch FSA action directly from Screen

--- packages/rnna/views/screen.js
+++ packages/rnna/views/screen.js
@@ -35,7 +35,7 @@ export default class Screen extends PureComponent {
       type = label;
     }
 
-    return dispatch(type, payload);
+    return dispatch({ type, payload });
   }
 
   set(key, val) {
