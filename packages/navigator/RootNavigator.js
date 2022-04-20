397ebd48fd2985a556fb65fe649f9f58ae696727
0x55 <0xfiftyfive@gmail.com>
2022-04-20 10:00:24 +0700

    Remove useless Root navigator

--- packages/navigator/RootNavigator.js
+++
@@ -1,9 +0,0 @@
-export default class RootNavigator {
-  constructor(navigators) {
-    super();
-
-    for (const [name, navigator] of Object.entries(navigators)) {
-      Object.assign(this, { [name]: navigator });
-    }
-  }
-}
