482d14db4e465db7b37201f428dc46bbbd6a2bdf
0x55 <0xfiftyfive@gmail.com>
2022-07-04 12:38:44 +0700

    app: useInput helper

--- packages/rnna/src/views/index.ts
+++ packages/rnna/src/views/index.ts
@@ -1,2 +1,20 @@
+import { useState } from 'react';
+
 export { createTheme } from 'react-native-themesheet';
 export { createCol, createRow } from 'react-native-col';
+
+export function useInput() {
+  const [value, setValue] = useState('');
+  const [isTouched, setTouched] = useState(false);
+
+  function handleChange(val: string) {
+    setTouched(true);
+    setValue(val);
+  }
+
+  return {
+    isTouched,
+    handleChange,
+    value,
+  };
+}
