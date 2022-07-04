73531c719dbbb93d76aa88a8f646d774f54b9fc4
0x55 <0xfiftyfive@gmail.com>
2022-07-04 12:06:59 +0700

    app: use Top Bar helper

--- packages/rnna/src/navigator/index.ts
+++ packages/rnna/src/navigator/index.ts
@@ -1,1 +1,26 @@
+// @ts-ignore
+import { Navigation } from 'react-native-navigation';
+import { useCallback, useEffect } from 'react';
+
 export * from '@rnna/navigator';
+
+export function useTopBar(id: string, cb: () => void) {
+  const handler = useCallback(
+    ({ buttonId }: { buttonId: string }) => {
+      if (buttonId === id) {
+        cb();
+      }
+    },
+    [cb, id],
+  );
+
+  useEffect(() => {
+    const listener = Navigation.events().registerNavigationButtonPressedListener(
+      handler,
+    );
+
+    return () => {
+      listener.remove();
+    };
+  }, [handler]);
+}
